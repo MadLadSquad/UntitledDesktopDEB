eaf54708a6d58cd789a6836b91475008e05720b3
Madman10K <stanv.contact176@gmail.com>
2025-08-29 00:23:01 +0000

    Add internal funcs library

--- packages/untitled-game-system-manager/after-install.sh
+++ packages/untitled-game-system-manager/after-install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mv /usr/lib64/libUntitledGameSystemManagerLib.*so* /usr/lib/
+mv /usr/lib64/libUntitledGameSystemManagerLib.*so* /usr/lib64/libUGM_Incus_InternalFuncs.so /usr/lib/
